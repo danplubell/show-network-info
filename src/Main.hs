41f12e74fc1ecf852929f2e1b1282aeabe4a8ff1
Dan Plubell <dplubell@cerner.com>
2015-05-08 13:51:20 -0500

    update cabal

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,29 @@
 module Main where
 import Network.Info
+import Network.BSD
+import Network.Socket.Internal
 
 main::IO()
 main = do
     ns <- getNetworkInterfaces
     mapM_ (putStr . showInterface) ns
+    hostname <- getHostName
+    hostentry <- getHostEntries False
+    putStrLn hostname   
+    print hostentry 
 
 showInterface::NetworkInterface-> String
 showInterface n = name n ++ "\n"
                ++ "  IPv4: " ++ show (ipv4 n) ++ "\n"
                ++ "  IPv6: " ++ show (ipv6 n) ++ "\n"
                ++ "  MAC:  " ++ show (mac n) ++ "\n"
+               
+getLocalHostIP::IO HostAddress
+getLocalHostIP = do
+    hostEntries <- getHostEntries False
+    return $ findLocalIP hostEntries
+
+findLocalIP::[HostEntry]->HostAddress
+findLocalIP =undefined
+
+    
