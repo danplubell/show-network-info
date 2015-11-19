82876798d54effdf70f80891a33ff454345739dc
Dan Plubell <dplubell@cerner.com>
2015-11-19 16:31:01 -0600

    add Data.IP and formatting of the raw ip address

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,37 @@
 module Main where
-import Network.Info
-import Network.BSD
-import Network.Socket.Internal
-
+import           Data.IP
+import           Network.BSD
+import qualified Network.Info            as NI
+import           Network.Socket.Internal
 main::IO()
 main = do
-    ns <- getNetworkInterfaces
+    ns <- NI.getNetworkInterfaces
+    putStrLn "Network Interfaces"
+    putStrLn "------------------"
     mapM_ (putStr . showInterface) ns
+
+    putStrLn "------------------"
     hostname <- getHostName
-    hostentry <- getHostEntries False
-    putStrLn hostname   
-    print hostentry 
+    putStrLn $ "HostName:  " ++  hostname
+    putStrLn "------------------"
+    putStrLn "Host Entries"
+    putStrLn "------------------"
+    hostentries <- getHostEntries False
+    mapM_ (putStr.showHostEntry) hostentries
 
-showInterface::NetworkInterface-> String
-showInterface n = name n ++ "\n"
-               ++ "  IPv4: " ++ show (ipv4 n) ++ "\n"
-               ++ "  IPv6: " ++ show (ipv6 n) ++ "\n"
-               ++ "  MAC:  " ++ show (mac n) ++ "\n"
-               
-getLocalHostIP::IO HostAddress
-getLocalHostIP = do
-    hostEntries <- getHostEntries False
-    return $ findLocalIP hostEntries
 
-findLocalIP::[HostEntry]->HostAddress
-findLocalIP =undefined
+showInterface::NI.NetworkInterface-> String
+showInterface n = NI.name n ++ "\n"
+               ++ "  IPv4: " ++ show (NI.ipv4 n) ++ "\n"
+               ++ "  IPv6: " ++ show (NI.ipv6 n) ++ "\n"
+               ++ "  MAC:  " ++ show (NI.mac n) ++ "\n"
 
-    
+showHostEntry::HostEntry -> String
+showHostEntry he =  show  (hostName he) ++ "\n"
+                   ++ "  Aliases:      " ++ show (hostAliases he) ++ "\n"
+                   ++ "  Family:       " ++ show (hostFamily he) ++ "\n"
+                   ++ "  Host Address: " ++ show  (map formatHostAddresses (hostAddresses he)) ++ "\n"
+   where formatHostAddresses h' =
+            case hostFamily he of
+              AF_INET6 -> show h'
+              _        -> show (fromHostAddress h')
